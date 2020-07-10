ec80e8774e28b6320712f197c6e8b0885245d556
Denis Kurochkin <dkurochkin@smartling.com>
2020-07-10 16:46:55 +0300

    lookup for SortOrder

--- src/main/java/org/zendesk/client/v2/model/SortOrder.java
+++ src/main/java/org/zendesk/client/v2/model/SortOrder.java
@@ -1,5 +1,7 @@
 package org.zendesk.client.v2.model;
 
+import java.util.Arrays;
+
 /**
  * An enum that represents sorting order. The name value maps to what the Zendesk API actually 
  * expects in the query param.
@@ -9,6 +11,13 @@ public enum SortOrder {
     ASCENDING("asc"), DESCENDING("desc");
 
     private final String queryParameter;
+
+    public SortOrder fromQueryParameter(String queryParameter) {
+        return Arrays.stream(SortOrder.values())
+                .filter(s -> s.getQueryParameter().equals(queryParameter))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("invalid queryparameter: " + queryParameter));
+    }
 
     SortOrder(String queryParameter) {
         this.queryParameter = queryParameter;
